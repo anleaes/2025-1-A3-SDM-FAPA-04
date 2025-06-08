f12e6bcf1c24fe8fa4e4084a6f492e2a23275bde
x578684@gruposantander.com <x578684@gruposantander.com>
2025-06-08 17:57:00 -0300

    add views dentro da classe media

---
+++ projetogestaocontratoapp/apps/media/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Media
+from rest_framework import viewsets
+from .serializer import MediaSerializer
+
+# Create your views here.
+class MediaSet(viewsets.ModelViewSet):
+    queryset = Media.objects.all()
+    serializer_class = MediaSerializer  
