ab821959d6f98ca35a099527203e5d01c8f38d86
x578684@gruposantander.com <x578684@gruposantander.com>
2025-06-08 15:52:10 -0300

    add serializer.py na classe client

---
+++ projetogestaocontratoapp/apps/client/serializer.py
@@ -0,0 +1,12 @@
+from .models import Client, ClientCommunication
+from rest_framework import serializers
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = '__all__'
+        
+class ClientCommunicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =ClientCommunication
+        fields = '__all__'
