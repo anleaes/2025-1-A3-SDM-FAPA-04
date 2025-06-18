9f1f98c9e9259c8031db95be00defdec676d5e61
x578684@gruposantander.com <x578684@gruposantander.com>
2025-06-18 14:03:03 -0300

    add apps em contracttypes

---
+++ projetogestaocontratoapp/apps/contracttypes/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ContractTypesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tipocontrato'
+    verbose_name = 'tiposcontrato'
